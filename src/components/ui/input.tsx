e16eacd4cabeb90e947a2cec52ae3153d1634640
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-11-22 19:20:01 -0300

    fix(input): fix bugs with style

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import { Box, InputProps, Input as KumaInput } from '@kuma-ui/core';
 
 interface Props extends InputProps {
@@ -7,7 +10,12 @@ interface Props extends InputProps {
 }
 
 export const Input = ({ id, placeholder, type, ...inputProps }: Props) => {
+  const [isFocused, setIsFocused] = useState(false);
+  const handleFocus = () => {
+    setIsFocused(!isFocused);
+  };
   const hasPlaceholder: boolean = placeholder !== '';
+
   return (
     <Box display="grid" gap="2px" width="100%" height="100%">
       {hasPlaceholder ? (
@@ -20,7 +28,18 @@ export const Input = ({ id, placeholder, type, ...inputProps }: Props) => {
       <KumaInput
         id={id}
         type={type}
-        className="bg-secondary border border-gray-900 text-foreground text-sm rounded-md focus:ring-primary focus:border-primary block w-full p-2.5"
+        display="block"
+        width="100%"
+        backgroundColor="hsl(var(--secondary))"
+        color="hsl(var(--foreground))"
+        borderWidth="1px"
+        borderColor={isFocused ? 'hsl(var(--primary))' : 'gray'}
+        borderRadius="var(--radius)"
+        outline={isFocused ? 'hsl(var(--primary)) solid 1px' : '0px'}
+        paddingY="5px"
+        paddingX="4px"
+        onFocus={handleFocus}
+        onBlur={handleFocus}
         required
         {...inputProps}
       />
