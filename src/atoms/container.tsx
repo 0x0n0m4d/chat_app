0fbcd2ac500b22464dd1e298acbe648254e2d82d
d3m0n <th3l1ghtd3m0n@gmail.com>
2024-11-22 18:47:46 -0300

    style: change values

--- src/atoms/container.tsx
+++ src/atoms/container.tsx
@@ -8,7 +8,7 @@ export const Container = ({ children, ...boxProps }: Props) => {
       maxWidth="100%"
       maxHeight="100%"
       width={['auto', 'auto', '723px', '933px', '1127px']}
-      height={['35rem', '100vh']}
+      height={['auto', '100vh']}
       marginLeft={['0em', '1em', 'auto']}
       marginRight={['0em', '1em', 'auto']}
       marginTop={['1em', '2em', '2.375em']}
